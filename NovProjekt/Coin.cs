a2a4cea821af07c1ffe06cf385315a49fff548d2
Clara01m <clara.mahlgard.sthlm.ntig@gmail.com>
2021-03-25 16:10:56 +0100

    Detta borde redan ha varit en commit

--- NovProjekt/Coin.cs
+++ NovProjekt/Coin.cs
@@ -6,7 +6,7 @@ namespace NovProjekt
     public class Coin
     {
 
-         public Rectangle gold = new Rectangle(350,500, 30, 30);
+        public Rectangle gold = new Rectangle(350,500, 30, 30);
         public Texture2D money = Raylib.LoadTexture("coin.png");
        
         public int value = 0;
--- NovProjekt/Program.cs
+++ NovProjekt/Program.cs
@@ -8,23 +8,27 @@ namespace NovProjekt
         enum GameScreens { Start, Game, GameOver }
         static void Main(string[] args)
         {
+            Raylib.InitWindow(900, 600, "November Falling");
             //Here are colors I want to use which aren't pre-existing in raylib
             Color ice = new Color(102, 204, 255, 255);
             Color ground = new Color(179, 230, 76, 255);
 
-
-            GameScreens screen = GameScreens.Start;
-            Raylib.InitWindow(900, 600, "November Falling");
-
+            Raylib.SetTargetFPS(60);  
+            
+            // spawning all characters I will use
             Player start = new Player();
             Enemy beanie = new Enemy(); 
-            Coin para = new Coin(); 
+            Coin para = new Coin();
+            Ground platform = new Ground();
+            
+            // starting console
+            GameScreens screen = GameScreens.Start;
             
             while (!Raylib.WindowShouldClose())
             {
-
                 //Intro screen
                 Raylib.BeginDrawing();
+
                 if (screen == GameScreens.Start)
                 {
                     //Drawing 
@@ -38,25 +42,19 @@ namespace NovProjekt
                     // Game starts
                     if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
                     {
-                        // Raylib.EndDrawing();
                         screen = GameScreens.Game;
-                        start = new Player();
                     }
                 }
                 //Actual Game
                 else if (screen == GameScreens.Game)
-                {
-                    // Player hello = new Player();
-                    Ground platform = new Ground();
-                    
-
+                {     
                     //DRAWING
                     Raylib.ClearBackground(ice);
                     Raylib.DrawRectangle(0, 550, 900, 50, ground);
-                    Raylib.DrawTexture(start.girl, (int) start.first.x, (int) start.first.y, Color.WHITE);
-                    Raylib.DrawTexture(beanie.bean, (int) beanie.beanMan.x, (int) beanie.beanMan.y, Color.WHITE);
-                    Raylib.DrawTexture(para.money, (int) para.gold.x, (int) para.gold.y, Color.WHITE);
-                     Raylib.DrawText("money= " + para.value, 50, 50, 50, Color.BLACK);
+                        Raylib.DrawTexture(start.girl, (int) start.first.x, (int) start.first.y, Color.WHITE);
+                        Raylib.DrawTexture(beanie.bean, (int) beanie.beanMan.x, (int) beanie.beanMan.y, Color.WHITE);
+                        Raylib.DrawTexture(para.money, (int) para.gold.x, (int) para.gold.y, Color.WHITE);
+                            Raylib.DrawText("money= " + para.value, 50, 50, 50, Color.BLACK);
 
 
 
@@ -97,14 +95,13 @@ namespace NovProjekt
                         if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
                         {
                             start.first.y -=3;
-
-
                         }
-                    }
+                   
                       else
                       {
                            start.first.y +=1; 
-                      }
+                      } 
+                    }
 
 
                 }
